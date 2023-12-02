d94832bf9f0d5251b21619f3f2b3bc2cd49bc72c
Damilare Aleji <alejigideonabidemi@gmail.com>
2023-12-02 10:14:34 +0100

    html and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,7 @@ const addEventOnElem = function (elem, type, callback) {
 /**
  * navbar toggle
  */
-
+ 
 const navbar = document.querySelector("[data-navbar]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 
