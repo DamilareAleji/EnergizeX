5e0b1db130d6f5dbbf24cfbd69970422f61a023d
Damilare Aleji <alejigideonabidemi@gmail.com>
2023-12-05 20:33:35 +0100

    final setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,3 +41,13 @@ addEventOnElem(navLinks, "click", closeNavbar);
 
 const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function() {
+    if (this.window.scrollY >= 100) {
+        header.classList.add("active");
+        backTopBtn.classList.add("active");
+    } else {
+        header.classList.remove("active");
+        backTopBtn.classList.remove("active");
+    }
+});
